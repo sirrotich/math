fd1098a7a11bc4fbded50501b3bf3703bea220c6
titus <rotichtitus12@gmail.com>
2019-02-16 14:53:04 +0300

    include description of the REAMEe.md file

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,23 @@
 function showResults(){
   var sides=[]
-  sides.push(document.getElementById("side1").value);
-  sides.push(document.getElementById("side2").value);
-  sides.push(document.getElementById("side3").value);
+  sides.push(document.getElementById('side1').value);
+  sides.push(document.getElementById('side2').value);
+  sides.push(document.getElementById('side3').value);
 
-  var side1 = parseInt(sides[0]);
-  var side2 = parseInt(sides[1]);
-  var side3 = parseInt(sides[3]);
+  var side1 =sides[0]
+  var side2 =sides[1]
+  var side3 =sides[2]
 
 if(side1 === side2 && side1 === side3 && side2 === side3){
-  output.innerHTML=("Equitorial triangle")
+output.innerHTML=("Equitorial triangle")
 }
 else if((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1){
-  ouput.innerHTML=("Sorry cant form a triangle")
+output.innerHTML=("Sorry cant form a triangle")
 }
 else if(side1 === side2 || side1 === side3 || side2 === side3){
-  output.innerHTML=("Isoceles triangle")
+output.innerHTML=("Isoceles triangle")
 }
 else if(side1 !== side2 && side1 !== side3 && side2 !== side3){
-  alert("scalene");
+output.innerHTML=("scalene");
 }
 }
-showResults();
