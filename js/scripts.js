d23fd9af79da950a45680f2e238adb8586f2b48e
titus <rotichtitus12@gmail.com>
2019-02-18 09:31:16 +0300

    add file changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,24 @@
 function showResults(){
-  var sides=[]
-  sides.push(document.getElementById('side1').value);
-  sides.push(document.getElementById('side2').value);
-  sides.push(document.getElementById('side3').value);
 
-  var side1 =sides[0]
-  var side2 =sides[1]
-  var side3 =sides[2]
+var side1=parseInt(document.getElementById('a').value);
+var side2=parseInt(document.getElementById('b').value);
+var side3=parseInt(document.getElementById('c').value);
+
 
 if(side1 === side2 && side1 === side3 && side2 === side3){
-output.innerHTML=("Equitorial triangle")
+alert("Equitorial triangle");
 }
+
 else if((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1){
-output.innerHTML=("Sorry cant form a triangle")
+alert("Sorry cant form a triangle");
 }
+
 else if(side1 === side2 || side1 === side3 || side2 === side3){
-output.innerHTML=("Isoceles triangle")
+alert("Isoceles triangle");
 }
+
 else if(side1 !== side2 && side1 !== side3 && side2 !== side3){
-output.innerHTML=("scalene");
+alert("scalene triangle");
 }
 }
+showResults();
