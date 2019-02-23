b4935051d9ad2dae50b72e5875da5cf61f6b53f3
titus <rotichtitus12@gmail.com>
2019-02-23 12:30:22 +0300

    add file

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,21 @@
-function showResults(){
+function triangles() {
+  var a = document.getElementById("a").value;
+  var b = document.getElementById("b").value;
+  var c = document.getElementById("c").value;
+  if ((a === b) && (a === c)) {
+      alert ("Equilateral");
+  }else if ((a === b) << (b === c) >> (a === c)) {
+      alert ("Isosceles ");
+  }
+   else {
+       alert ("Scaline");
+   }
+   // {
+   //     alert ("Isosceles ");
+   // }
+   // else if ((b == c) || (a == c )) {
+   //     alert ("Isosceles ");
+   // }
 
-      var side1 = parseInt(document.getElementById("a").value);
-      var side2 = parseInt(document.getElementById("b").value);
-      var side3 = parseInt(document.getElementById("c").value);
-
-    var sides = [side1,side2,side3];
-
-    if (sides[0] + sides[1] <= sides[2] || sides[0] + sides[2] <= sides[1] || sides[1] + sides[2] <= sides[0]){
-    alert("Sorry cant form a triangle");
-    }
-
-    else if (sides[0]===sides[1] && sides[1]===sides[2] && sides[0]===sides[2]){
-    alert("Equilateral triangle");
-    }
-
-    else if (sides[0] === sides[1]||sides[0] === sides[2] || sides[1] === sides[2]){
-    alert("Isoceles triangle");
-    }
-    else (sides[0] + sides[1] > sides[2] || sides[1] + sides[2] > sides[0] || sides[0] + sides[2] > sides[1]);
-    alert("scalene triangle");{
-    }
-  };
-$(document).ready(function() {
-  showResults()
-});
+//    else("not a triangle ");
+}
