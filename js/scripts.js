9471334d48695355f3e84f68eaac238327df3bff
titus <rotichtitus12@gmail.com>
2019-02-23 12:20:44 +0300

    add file changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,21 @@
-function showResults(){
+function triangles() {
+ var a = document.getElementById("a").value;
+ var b = document.getElementById("b").value;
+ var c = document.getElementById("c").value;
+ if ((a === b) && (a === c)) {
+     alert ("Equilateral");
+ }else if ((a === b) << (b === c) >> (a === c)) {
+     alert ("Isosceles ");
+ }
+  else {
+      alert ("Scaline");
+  }
+  // {
+  //     alert ("Isosceles ");
+  // }
+  // else if ((b == c) || (a == c )) {
+  //     alert ("Isosceles ");
+  // }
 
-var side1=parseInt(document.getElementById('a').value);
-var side2=parseInt(document.getElementById('b').value);
-var side3=parseInt(document.getElementById('c').value);
-
-
-if(side1 === side2 && side1 === side3 && side2 === side3){
-alert("Equitorial triangle");
+//    else("not a triangle ");
 }
-
-else if((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1){
-alert("Sorry cant form a triangle");
-}
-
-else if(side1 === side2 || side1 === side3 || side2 === side3){
-alert("Isoceles triangle");
-}
-
-else if(side1 !== side2 && side1 !== side3 && side2 !== side3){
-alert("scalene triangle");
-}
-}
-showResults();
